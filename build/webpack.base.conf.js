e2ecc084c980716afcccddcc570d580bc8a504c4
tsumugu <tsumugu1515@icloud.com>
2020-01-11 00:38:39 +0900

    Edit somefiles And Add test.html(Snap.svg)

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,11 +36,16 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      snapsvg: 'snapsvg/dist/snap.svg.js',
     }
   },
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
+      {
+        test: require.resolve('snapsvg/dist/snap.svg.js'),
+        use: 'imports-loader?this=>window,fix=>module.exports=0',
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
