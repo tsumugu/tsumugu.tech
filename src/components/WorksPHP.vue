6cdbd4a1b0254ed09887d4bdc84b3679f5dde3d9
tsumugu <tsumugu1515@icloud.com>
2020-01-12 23:51:10 +0900

    Change WorksPHP.vue Add Firebase

--- src/components/WorksPHP.vue
+++ src/components/WorksPHP.vue
@@ -1,8 +1,41 @@
 <template>
-   <p>作品PHP</p>
+<div>
+  <h1>作品PHP</h1>
+  <ul>
+    <li id="itemlist" v-for="(item, key) in items" :key="key">
+      {{item.title}}
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
+import db from '../../config/firebaseInit'
+export default {
+  data () {
+    return {
+      items: [],
+      loading: true
+    }
+  },
+  created () {
+    db.collection('contacts').get().then((querySnapshot) => {
+      this.loading = false
+      querySnapshot.forEach((doc) => {
+        let data = {
+          'id': doc.id,
+          'thumbnail': doc.data().thumbnail,
+          'title': doc.data().title,
+          'siteurl': doc.data().siteurl,
+          'description': doc.data().description,
+          'Lang': doc.data().Lang,
+          'kdwr': doc.data().kdwr
+        }
+        this.items.push(data)
+      })
+    })
+  }
+}
 </script>
 
 <style>
