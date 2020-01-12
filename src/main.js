e4d11a3f11ddc376f5a2cac7966acbf1b030f9fe
tsumugu <tsumugu1515@icloud.com>
2020-01-12 11:38:39 +0900

    Change Tree.vue Copy functions from test.html

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+const snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  snap,
   router,
   components: { App },
   template: '<App/>'
