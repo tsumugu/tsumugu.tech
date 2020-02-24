71bf0a5bf68c77c7feb84bfbea2b87bc3ddd640e
tsumugu <tsumugu1515@icloud.com>
2020-02-24 13:09:12 +0900

    Change Top.vue add matter-js

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ JavaScriptフレームワーク
 ④ Buildしたファイルを公開ディレクトリにコピー(```Deploy```)
 
 > ![exp](https://user-images.githubusercontent.com/29032673/72495403-ed2b9780-3869-11ea-8147-e540521560c6.png)
+## Windows Github Push BAT
+```
+> gitpush.bat "commit name"
+```
 ## grpcで落ちるとき
 ```
 sudo npm install grpc@1.24.2 --unsafe-perm
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -6,43 +6,170 @@
 </template>
 
 <script>
-import Matter from 'matter-js'
+var Matter = require('matter-js')
+var Engine = Matter.Engine,
+    Events = Matter.Events,
+    Runner = Matter.Runner,
+    Render = Matter.Render,
+    World = Matter.World,
+    Body = Matter.Body,
+    Mouse = Matter.Mouse,
+    Common = Matter.Common,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Bodies = Matter.Bodies
+
 export default {
   data: function () {
     return {
       canvas: null,
+      engine: null,
+      render: null,
       dimensions: null
     }
   },
+  methods: {
+    handleResize: function() {
+      let dimensions = {};
+          dimensions.width  = window.innerWidth;
+          dimensions.height = window.innerHeight;
+      // Engine.clear(this.engine)
+      this.render.canvas.width = window.innerWidth;
+      this.render.canvas.height = window.innerHeight;
+      this.dimensions = dimensions
+    }
+  },
   mounted() {
+    let dimensions = {}
+        dimensions.width  = window.innerWidth
+        dimensions.height = window.innerHeight
+    this.dimensions = dimensions
     this.canvas = this.$refs.canvas
 
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        World = Matter.World,
-        Bodies = Matter.Bodies;
-    var engine = Engine.create(),
-        world = engine.world;
-    var render = Render.create({
+    this.engine = Engine.create()
+    var world = this.engine.world
+        world.gravity.y = 0
+        world.gravity.x = 0
+        world.gravity.scale = 0
+
+    this.render = Render.create({
       element: this.canvas,
-      engine: engine,
+      engine: this.engine,
       options: {
-        background: '#FFFFFF',
-        wireframes: false
+        showVelocity: false,
+        width: this.dimensions.width,
+        height: this.dimensions.height,
+        wireframes: false,
+        background: 'rgb(240,240,240)'
       }
     })
-    Render.run(render)
     var runner = Runner.create()
-    Runner.run(runner, engine)
 
-    //オブジェクト作成
-    var floor = Bodies.rectangle(400, 350, 800, 30, {
-      isStatic: true
-    })
-    var square = Bodies.rectangle(500, 0, 100, 80)
-    var circle = Bodies.circle(200, 0, 50)
-    World.add(world, [floor, square, circle])
+/*
+    var attractiveBody = Bodies.circle(
+      this.render.options.width / 2,
+      this.render.options.height / 2,
+      (Math.max(dimensions.width / 4, dimensions.height / 4)) / 2,
+      {
+        render: {
+          fillStyle: `rgb(240,240,240)`,
+          strokeStyle: `rgb(240,240,240)`,
+          lineWidth: 0
+        },
+        isStatic: true,
+        plugin: {
+          attractors: [
+            function(bodyA, bodyB) {
+              return {
+                x: (bodyA.position.x - bodyB.position.x) * 1e-6,
+                y: (bodyA.position.y - bodyB.position.y) * 1e-6,
+              };
+            }
+          ]
+        }
+      });
+
+    World.add(world, attractiveBody);
+
+    // add mouse control
+    var mouse = Mouse.create(this.render.canvas);
+
+    Events.on(this.engine, 'afterUpdate', function() {
+        if (!mouse.position.x) return;
+        // smoothly move the attractor body towards the mouse
+        Body.translate(attractiveBody, {
+            x: (mouse.position.x - attractiveBody.position.x) * 0.12,
+            y: (mouse.position.y - attractiveBody.position.y) * 0.12
+        });
+    });
+*/
+        // add some bodies that to be attracted
+        for (var i = 0; i < 60; i += 1) {
+          let x = Common.random(0, this.render.options.width);
+          let y = Common.random(0, this.render.options.height);
+          let s = Common.random() > 0.6 ? Common.random(10, 80) : Common.random(4, 60);
+          let poligonNumber= Common.random(3, 6);
+          var body = Bodies.polygon(
+            x,y,
+            poligonNumber,
+            s,
+
+            {
+              mass: s / 20,
+              friction: 0,
+              frictionAir: 0.02,
+              angle: Math.round(Math.random() * 360),
+              render: {
+                fillStyle: '#FFFFFF',
+                strokeStyle: `#DDDDDD`,
+                lineWidth: 2
+              }
+            }
+          );
+
+          World.add(world, body);
+
+
+          let r = Common.random(0,1)
+          var circle = Bodies.circle(x, y, Common.random(2, 8), {
+                mass: 0.1,
+                friction: 0,
+                frictionAir: 0.01,
+                render: {
+                  fillStyle: r > 0.3 ? `#FF2D6A` : `rgb(240,240,240)`,
+                  strokeStyle: `#E9202E`,
+                  lineWidth: 2
+                }
+              });
+          World.add(world, circle);
+
+          var circle = Bodies.circle(x, y, Common.random(2, 20), {
+                mass: 6,
+                friction: 0,
+                frictionAir: 0,
+                render: {
+                  fillStyle: r > 0.3 ? `#4267F8` : `rgb(240,240,240)`,
+                  strokeStyle: `#3257E8`,
+                  lineWidth: 4
+                }
+              });
+          World.add(world, circle);
+
+          var circle = Bodies.circle(x, y, Common.random(2, 30), {
+                mass: 0.2,
+                friction: 0.6,
+                frictionAir: 0.8,
+                render: {
+                  fillStyle: `rgb(240,240,240)`,
+                  strokeStyle: `#FFFFFF`,
+                  lineWidth: 3}
+              });
+          World.add(world, circle);
+        }
+
+        Runner.run(runner, this.engine);
+        Render.run(this.render);
+    // window.addEventListener('resize', this.handleResize)
   }
 }
 </script>
