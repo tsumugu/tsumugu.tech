3ea155497fe6be6d78c897e3c34b54946c63cea2
tsumugu <tsumugu1515@icloud.com>
2020-02-28 02:03:07 +0900

    Change Top.vue

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -52,6 +52,8 @@ export default {
 
     this.engine = Engine.create()
     var world = this.engine.world
+        world.gravity.y = 0
+        world.gravity.x = 0
         world.gravity.scale = 0
 
     this.render = Render.create({
@@ -65,16 +67,18 @@ export default {
         background: 'rgb(240,240,240)'
       }
     })
-
     var runner = Runner.create()
-    Runner.run(runner, this.engine)
-    Render.run(this.render)
 
     var attractiveBody = Bodies.circle(
       this.render.options.width / 2,
       this.render.options.height / 2,
-      50,
+      (Math.max(dimensions.width / 4, dimensions.height / 4)) / 3,
       {
+        render: {
+          fillStyle: `#82b349`,
+          strokeStyle: `#DDDDDD`,
+          lineWidth: 0
+        },
         isStatic: true,
         plugin: {
           attractors: [
@@ -82,33 +86,76 @@ export default {
               return {
                 x: (bodyA.position.x - bodyB.position.x) * 1e-6,
                 y: (bodyA.position.y - bodyB.position.y) * 1e-6,
-              }
+              };
             }
           ]
         }
-      })
+      });
 
     World.add(world, attractiveBody);
-    for (var i = 0; i < 300; i += 1) {
-      var body = Bodies.polygon(
-        Common.random(0, this.render.options.width),
-        Common.random(0, this.render.options.height),
-        Common.random(1, 5),
-        Common.random() > 0.9 ? Common.random(15, 25) : Common.random(5, 10)
-      );
-      World.add(world, body);
-    }
+        // add some bodies that to be attracted
+        for (var i = 0; i < 80; i += 1) {
+          let x = Common.random(0, this.render.options.width);
+          let y = Common.random(0, this.render.options.height);
+          let s = Common.random() > 0.6 ? Common.random(10, 80) : Common.random(4, 60);
+          let poligonNumber= Common.random(3, 6);
+          var body = Bodies.polygon(
+            x,y,
+            poligonNumber,
+            s,
 
-    var mouse = Mouse.create(this.render.canvas);
-    Events.on(this.engine, 'afterUpdate', function() {
-      if (!mouse.position.x) {
-        return;
-      }
-      Body.translate(attractiveBody, {
-        x: (mouse.position.x - attractiveBody.position.x) * 0.18,
-        y: (mouse.position.y - attractiveBody.position.y) * 0.18
-      });
-    });
+            {
+              mass: s / 20,
+              friction: 0,
+              frictionAir: 0.02,
+              angle: Math.round(Math.random() * 360),
+              render: {
+                fillStyle: '#FFFFFF',
+                strokeStyle: `#DDDDDD`,
+                lineWidth: 2
+              }
+            }
+          );
+          World.add(world, body);
+
+          var circle = Bodies.circle(x, y, Common.random(2, 20), {
+                mass: 6,
+                friction: 0,
+                frictionAir: 0,
+                render: {
+                  fillStyle: '#FFFFFF',
+                  strokeStyle: `#DDDDDD`,
+                  lineWidth: 2
+                }
+              });
+          World.add(world, circle);
+
+          var circle = Bodies.circle(x, y, Common.random(2, 30), {
+                mass: 0.2,
+                friction: 0.3,
+                frictionAir: 0.5,
+                render: {
+                  fillStyle: '#FFFFFF',
+                  strokeStyle: `#DDDDDD`,
+                  lineWidth: 3
+                }
+              });
+          World.add(world, circle);
+        }
+
+        // add mouse control
+        var mouse = Mouse.create(this.render.canvas);
+        Events.on(this.engine, 'afterUpdate', function() {
+            if (!mouse.position.x) return;
+            // smoothly move the attractor body towards the mouse
+            Body.translate(attractiveBody, {
+                x: (mouse.position.x - attractiveBody.position.x) * 0.18,
+                y: (mouse.position.y - attractiveBody.position.y) * 0.18
+            });
+        });
+
+        Runner.run(runner, this.engine);
+        Render.run(this.render);
     window.addEventListener('resize', this.handleResize)
   }
 }
