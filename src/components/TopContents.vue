be318e5cd8e815804a51c56b1c5ef667e7cc5dd9
tsumugu <tsumugu1515@icloud.com>
2020-03-14 22:21:46 +0900

    Change Top Add top image

--- src/components/TopContents.vue
+++ src/components/TopContents.vue
@@ -2,9 +2,9 @@
   <div id="top-wrap">
     <div id="top-main-wrap">
       <div id="top-bgcolor"></div>
-      <div id="top-bg-img" v-bind:style="{ backgroundImage: 'url(' + image + ')' }"></div>
-      <div id="top-name">Tsumugu Yamaguchi</div>
-      <div id="top-cover"><input type="file" id="files" name="files[]" multiple /></div>
+      <div id="top-bg-img"></div>
+      <div id="top-name"></div>
+      <div id="top-cover"></div>
     </div>
   </div>
 </template>
@@ -13,29 +13,11 @@
 export default {
   data() {
     return {
-      image: null
     }
   },
   methods: {
-    handleFileSelect(evt) {
-      var files = evt.target.files;
-      var _this = this
-      for (var i = 0, f; f = files[i]; i++) {
-        if (!f.type.match('image.*')) {
-          continue
-        }
-        var reader = new FileReader()
-        reader.onload = (function(theFile) {
-          return function(e) {
-            _this.image = e.target.result
-          };
-        })(f)
-        reader.readAsDataURL(f)
-      }
-    }
   },
   mounted() {
-    document.getElementById('files').addEventListener('change', this.handleFileSelect, false)
   }
 }
 </script>
@@ -59,26 +41,15 @@ export default {
   height: 100%;
   z-index: 2;
 
-  background-image: url("https://tsumugu.s3-ap-northeast-1.amazonaws.com/TOPPC-RSZD.jpg");
   background-size: cover;
   background-attachment: fixed;
-  background-position: center top;
 }
 
 #top-name {
   position: absolute;
-
-  top: 50%;
-  margin-left: 20px;
-  text-align: left;
-  /*
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  */
   color: #ffffff;
-  font-size: xxx-large;
+  font-size: 2.5em;
+  opacity: 0.9;
   z-index: 3;
 }
 #top-cover {
@@ -87,16 +58,36 @@ export default {
   height: 100%;
   z-index: 4;
 }
+@media (max-width: 3000px) and (min-width: 600px) {
+  #top-bg-img {
+    /* TOPPC-RSZD.jpg */
+    background-image: url("https://tsumugu.s3-ap-northeast-1.amazonaws.com/TOPPC.jpg");
+    background-position: center top;
+  }
+  #top-name {
+    top: 50%;
+    margin-left: 10%;
+    text-align: left;
+  }
+  #top-name:before {
+    content: "Tsumugu Yamaguchi";
+  }
+}
 /* SP */
 @media screen and (max-width:600px) {
   #top-bg-img {
-    background-image: url("https://tsumugu.s3-ap-northeast-1.amazonaws.com/1242_2688.jpg");
+    background-image: url("https://tsumugu.s3-ap-northeast-1.amazonaws.com/TOPSP.jpg");
     background-position: center center
   }
   #top-name {
-    top: 10%;
+    /*bottom: 230px;*/
+    bottom: 35%;
     margin-left: 20px;
     text-align: left;
+  }
+  #top-name:before {
+    content: "Tsumugu\AYamaguchi";
+    white-space: pre;
   }
 }
 </style>
