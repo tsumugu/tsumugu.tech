e90add00ca465d0832acc9cdf9ee7458cf57927f
tsumugu <tsumugu1515@icloud.com>
2020-03-17 23:22:53 +0900

    Change PresenScrollCounter And Top

--- src/components/PresenScrollCounter.vue
+++ src/components/PresenScrollCounter.vue
@@ -43,6 +43,8 @@ export default {
   data() {
     return {
       beforeCalltime: 0,
+      swipeY: 0,
+      swipeY_S: 0,
       beforeTypo: 0
     }
   },
@@ -59,42 +61,91 @@ export default {
   methods: {
     treeUpdateEvt: function(value) {
       this.isAnimating = value
+    },
+    touchHandlerS(event) {
+      var x = 0, y = 0;
+      if (event.touches && event.touches[0]) {
+        x = event.touches[0].clientX;
+        y = event.touches[0].clientY;
+      } else if (event.originalEvent && event.originalEvent.changedTouches[0]) {
+        x = event.originalEvent.changedTouches[0].clientX;
+        y = event.originalEvent.changedTouches[0].clientY;
+      } else if (event.clientX && event.clientY) {
+        x = event.clientX;
+        y = event.clientY;
+      }
+      this.swipeY_S = y
+    },
+    touchHandlerM(event) {
+      var x = 0, y = 0;
+      if (event.touches && event.touches[0]) {
+        x = event.touches[0].clientX;
+        y = event.touches[0].clientY;
+      } else if (event.originalEvent && event.originalEvent.changedTouches[0]) {
+        x = event.originalEvent.changedTouches[0].clientX;
+        y = event.originalEvent.changedTouches[0].clientY;
+      } else if (event.clientX && event.clientY) {
+        x = event.clientX;
+        y = event.clientY;
+      }
+      this.swipeY = y
+      // console.log('y : ' + y);
+    },
+    touchHandlerE(event) {
+      this.scrollCountEvent(this.swipeY_S-this.swipeY)
+      this.touchHandlerR()
+    },
+    touchHandlerR() {
+      //reset
+      this.swipeY = 0
+      this.swipeY_S = 0
+    },
+    scrollCountEvent: function(deltaY) {
+      //前回から時間経ってるかチェック
+      var nowtimestamp = new Date().getTime()
+      var defTime = nowtimestamp-this.beforeCalltime
+      //400, 100
+      if (defTime > 400 && Math.abs(deltaY)>90) {
+        console.log(deltaY, event)
+        var dis = null
+        if (deltaY == -0) {
+          dis = -1
+        } else {
+          dis = deltaY
+        }
+        if (dis > 0) {
+          //console.log("Plus")
+          if (2>this.pageNum&&this.pageNum>=0) {
+            this.pageNum += 1
+            this.pageWcNum = (this.pageNum*10)+1
+          }
+        } else {
+          //console.log("Minus")
+          if (2>=this.pageNum&&this.pageNum>0) {
+            this.pageNum -= 1
+            this.pageWcNum = (this.pageNum*10)+1
+          }
+        }
+      }
+      this.beforeCalltime = new Date().getTime()
+      //
     }
   },
   mounted() {
     var _this = this
     const targetElement = this.$refs.treespacer
-    // TODO: wheelに対応しているかチェックする
-    window.addEventListener('wheel', function(event) {
-      //前回から時間経ってるかチェック
-      var nowtimestamp = new Date().getTime()
-      var defTime = nowtimestamp-_this.beforeCalltime
-      //400, 100
-      if (defTime > 400 && Math.abs(event.deltaY)>90) {
-        console.log(event.deltaY, event)
-        var dis = null
-        if (event.deltaY == -0) {
-          dis = -1
-        } else {
-          dis = event.deltaY
-        }
-        if (dis > 0) {
-          //console.log("Plus")
-          if (2>_this.pageNum&&_this.pageNum>=0) {
-            _this.pageNum += 1
-            _this.pageWcNum = (_this.pageNum*10)+1
-          }
-        } else {
-          //console.log("Minus")
-          if (2>=_this.pageNum&&_this.pageNum>0) {
-            _this.pageNum -= 1
-            _this.pageWcNum = (_this.pageNum*10)+1
-          }
-        }
-      }
-      _this.beforeCalltime = new Date().getTime()
-      //
-    }, { passive: false })
+    var isAvailableTouch = 'ontouchend' in window
+    // isAvailableTouchがTrueだったらそっちを優先
+    if (isAvailableTouch) {
+      window.addEventListener('touchstart', this.touchHandlerS, false);
+      window.addEventListener('touchmove', this.touchHandlerM, false);
+      window.addEventListener('touchend', this.touchHandlerE, false);
+      window.addEventListener('touchcancel', this.touchHandlerR, false);
+    } else {
+      window.addEventListener('wheel', function(event) {
+        _this.scrollCountEvent(event.deltaY)
+      }, { passive: false })
+    }
   }
 }
 </script>
