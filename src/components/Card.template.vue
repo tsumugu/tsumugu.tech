71958fbc479142f34cf36a78430acc95221d9bac
tsumugu <tsumugu1515@icloud.com>
2020-11-01 18:18:07 +0900

    Card.vue 順番変更

--- src/components/Card.template.vue
+++ src/components/Card.template.vue
@@ -2,8 +2,8 @@
   <div class="card" v-bind:id="item.id">
     <!-- 依存あり -->
     <div class="card__contentsWrapper">
-      <progressive-img v-show="!isLimit" class="card__contentsWrapper__img" v-bind:src="item.thumbnail" />
       <h2 class="card__contentsWrapper__title">{{item.title}}</h2>
+      <progressive-img v-show="!isLimit" class="card__contentsWrapper__img" v-bind:src="item.thumbnail" />
       <p class="card__contentsWrapper__description" v-show="!isLimit&&item.description!=''" v-html="item.description"></p>
       <div class="card__contentsWrapper__tags">
         <div v-show="isDispKadai">
@@ -15,16 +15,16 @@
             <div class="card__tag__arrow--red" v-show="item.solution!=''" v-bind:class="{ cardTagsArrowMargin: item.targetUser!=''&&item.problem!='' }"><font-awesome-icon icon="arrow-down" size="lg" /></div>
             <div class="card__tag--red" v-show="item.solution!=''"><p class="card__tag__title--red">解決 <font-awesome-icon icon="grin-beam" /></p><p class="card__tag__val--red">{{item.solution}}</p></div>
           </div>
+          <div class="card__tag--green" v-show="item.whatLearned!=''"><p class="card__tag__title--study">学習 <font-awesome-icon icon="school" /></p><p class="card__tag__val--study">{{item.whatLearned}}</p></div>
+          <div class="card__tag" v-show="item.genle!=''"><p class="card__tag__title">環境 <font-awesome-icon icon="mobile-alt" /></p><p class="card__tag__val">{{item.genle}}</p></div>
+          <div class="card__tag" v-show="item.allLang!=''"><p class="card__tag__title">言語 <font-awesome-icon icon="code" /></p><p class="card__tag__val">{{item.allLang}}</p></div>
         </div>
-        <div class="card__tag--green" v-show="item.whatLearned!=''"><p class="card__tag__title--study">学習 <font-awesome-icon icon="school" /></p><p class="card__tag__val--study">{{item.whatLearned}}</p></div>
-        <div class="card__tag" v-show="item.genle!=''"><p class="card__tag__title">環境 <font-awesome-icon icon="mobile-alt" /></p><p class="card__tag__val">{{item.genle}}</p></div>
-        <div class="card__tag" v-show="item.allLang!=''"><p class="card__tag__title">言語 <font-awesome-icon icon="code" /></p><p class="card__tag__val">{{item.allLang}}</p></div>
       </div>
       <p class="card__contentsWrapper__kdwr" v-show="item.kdwr!=''&&item.kdwr!=null">{{item.kdwr}}</p>
       <div v-show="isDispEdit&&isLogin"><button @click="oepnEdit(item.id)" class="card__button card__button--edit">Edit</button></div>
       <!-- 依存あるかも: #card-button-wrapper -->
       <div class="card__contentsWrapper__buttonWrapper" v-show="!isLimit">
-        <button @click="cardButtonEv(item.siteurl, item.id)" class="card__button card__button--read wid50per" v-bind:disabled="!item.isDispReadButton"><font-awesome-icon icon="book-reader" /> 詳しく読む</button>
+        <button @click="cardButtonEv(item.siteurl, item.id)" class="card__button card__button--read wid50per" v-bind:disabled="!item.isDispReadButton"><font-awesome-icon icon="book-reader" /> 説明を読む</button>
         <button @click="goToSite(item.siteurl)" class="card__button card__button--gosite wid50per" v-bind:disabled="!item.isDispGotoSiteButton"><font-awesome-icon icon="external-link-alt" /> サイトを開く</button>
       </div>
       <slot></slot>
@@ -68,11 +68,11 @@ export default {
 .card {
   max-width: 760px;
   border-radius: 25px;
-  padding: 15px 10px 15px 10px;
+  padding: 15px;
   box-shadow: 0 0 5px 3px rgba(204, 204, 204, 0.2);
   background-color: $card-background;
   &__contentsWrapper__img {
-    border-radius: 25px;
+    border-radius: 10px;
     z-index: 1;
     border: 1px solid $card-border;
   }
